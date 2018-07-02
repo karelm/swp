311f9484facb537a15c5da13f126d0ae1fed5fb0
mcgrailka <mcgrailka@gmail.com>
2018-07-02 22:49:39 +0100

    First version of site

---
+++ js/script.js
@@ -0,0 +1,31 @@
+/*
+
+Style   : MobApp Script JS
+Version : 1.0
+Author  : Surjith S M
+URI     : https://surjithctly.in/
+
+Copyright © All rights Reserved 
+
+*/
+
+$(function() {
+    "use strict";
+    /*-----------------------------------
+     * NAVBAR CLOSE ON CLICK
+     *-----------------------------------*/
+
+    $('.navbar-nav > li:not(.dropdown) > a').on('click', function() {
+        $('.navbar-collapse').collapse('hide');
+    });
+    /* 
+     * NAVBAR TOGGLE BG
+     *-----------------*/
+    var siteNav = $('#navbar');
+    siteNav.on('show.bs.collapse', function(e) {
+        $(this).parents('.nav-menu').addClass('menu-is-open');
+    })
+    siteNav.on('hide.bs.collapse', function(e) {
+        $(this).parents('.nav-menu').removeClass('menu-is-open');
+    })
+}); /* End Fn */
