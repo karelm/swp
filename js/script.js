6457ee60d80d5d3b178eab8535a18de7ffd05dc2
mcgrailka <mcgrailka@gmail.com>
2018-07-09 21:00:51 +0100

    Updating content for site

--- js/script.js
+++ js/script.js
@@ -14,7 +14,6 @@ $(function() {
     /*-----------------------------------
      * NAVBAR CLOSE ON CLICK
      *-----------------------------------*/
-
     $('.navbar-nav > li:not(.dropdown) > a').on('click', function() {
         $('.navbar-collapse').collapse('hide');
     });
